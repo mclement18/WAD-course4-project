862fce1def9e78c26bd0168865bd383266bf58df
Mathieu Clément <mcfly1893@gmail.com>
2020-01-06 00:12:30 +0100

    Extract avatar links in constant and use case statement in ApplicationHelper#default_avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,20 @@
 module ApplicationHelper
+  AVATAR_1 = "https://68.media.tumblr.com/7d65a925636d6e3df94e2ebe30667c29/tumblr_nq1zg0MEn51qg6rkio1_500.jpg"
+  AVATAR_2 = "https://68.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png"
+  AVATAR_3 = "http://78.media.tumblr.com/75bad14fee104f69652084d545213291/tumblr_mldfty8fh41qcnibxo5_1280.png"
+  AVATAR_4 = "https://68.media.tumblr.com/22d1c50c3e2ca1062a94b47a65bfeb6d/tumblr_o51oavbMDx1ugpbmuo10_500.png"
+  
   def default_avatar(user)
     if user.name.present?
-      if user.name[0].downcase < 'h'
-        "https://68.media.tumblr.com/7d65a925636d6e3df94e2ebe30667c29/tumblr_nq1zg0MEn51qg6rkio1_500.jpg"
-      elsif user.name[0].downcase < 'n'
-        "https://68.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png"
-      elsif user.name[0].downcase < 'u'
-        "http://78.media.tumblr.com/75bad14fee104f69652084d545213291/tumblr_mldfty8fh41qcnibxo5_1280.png"
-      else
-        "https://68.media.tumblr.com/22d1c50c3e2ca1062a94b47a65bfeb6d/tumblr_o51oavbMDx1ugpbmuo10_500.png"
+      first_letter = user.name[0].downcase
+      case
+      when first_letter < 'h' then AVATAR_1
+      when first_letter < 'n' then AVATAR_2
+      when first_letter < 'u' then AVATAR_3
+      else AVATAR_4
       end
     else
-      "https://68.media.tumblr.com/7d65a925636d6e3df94e2ebe30667c29/tumblr_nq1zg0MEn51qg6rkio1_500.jpg"
+      AVATAR_1
     end
   end
 
