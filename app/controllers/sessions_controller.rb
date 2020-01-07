62f8c5f50c5db4cd3a5760dfd1ad03ee7e07b51a
Mathieu Clément <mcfly1893@gmail.com>
2020-01-07 17:41:01 +0100

    Implement log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+
+    respond_to do |format|
+      if @user.present? && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+
+        format.html { redirect_to root_url, notice: 'Successfully logged in!' }
+      else
+        format.html { redirect_to new_session_url, alert: 'Invalid email and password combiantion. Please try again.' }
+      end
+    end
   end
 
   def destroy
