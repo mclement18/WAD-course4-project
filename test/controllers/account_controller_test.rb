b3715232d6501737a08d98a87f44d13f8ff5f428
Mathieu Clément <mcfly1893@gmail.com>
2020-01-09 19:22:23 +0100

    Add test for AccountController

--- test/controllers/account_controller_test.rb
+++ test/controllers/account_controller_test.rb
@@ -3,12 +3,21 @@ require 'test_helper'
 class AccountControllerTest < ActionDispatch::IntegrationTest
   setup do
     app.default_url_options[:locale] = :en
-    @user = users(:one)
-    sign_in_as('one@extensionschool.ch')
+    sign_in_as('two@extensionschool.ch')
   end
 
   test "should get tips owned by current user" do
     get account_tips_url
     assert_response :success
+  end
+
+  test "should get edit" do
+    get account_url
+    assert_response :success
+  end
+
+  test "should update current user details" do
+    patch account_url, params: { user: { email: 'new@extensionschool.ch', name: 'New Name', avatar_url: 'url/to/avatar' } }
+    assert_redirected_to account_url
   end
 end
