8db73da51721152910fe39dec340f83e087fa153
Mathieu Clément <mcfly1893@gmail.com>
2020-01-09 12:59:58 +0100

    Add test for Comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comment must have body" do
+    comment = Comment.new user: users(:one),
+                          tip: tips(:one)
+    refute comment.valid?
+  end
+
+  test "comment must have user" do
+    comment = Comment.new body: 'New comment',
+                          tip: tips(:one)
+    refute comment.valid?
+  end
+
+  test "comment must have tip" do
+    comment = Comment.new body: 'New comment',
+                          user: users(:one)
+    refute comment.valid?
+  end
 end
