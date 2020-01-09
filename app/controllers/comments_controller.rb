261ce9bf1d3ba389770a5ec22d5c815e32c9c071
Mathieu Clément <mcfly1893@gmail.com>
2020-01-09 15:04:54 +0100

    Assign current user to created comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.tip = @tip
-
-    # Needs to be changed: this always sets the User for a Comment to the first
-    # User in the database, _not_ the User that POSTed the Comment.
-    @comment.user = User.first
+    @comment.user = current_user
 
     respond_to do |format|
       # In these format calls, the flash message is being passed directly to
