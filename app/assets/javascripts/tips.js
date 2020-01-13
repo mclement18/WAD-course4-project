1664b2af734a26fb81af81a212d74b7bdd3bf5db
Mathieu Clément <mcfly1893@gmail.com>
2020-01-13 11:56:52 +0100

    Display not tips message in account/tips#index when delete last tip via AJAX

--- app/assets/javascripts/tips.js
+++ app/assets/javascripts/tips.js
@@ -1,5 +1,18 @@
 const Tip = {};
 
-Tip.removeFromPage = function(tipId) {
+Tip.removeFromPage = function(tipId, noTipsMessage) {
   document.getElementById('tip-' + tipId).remove();
+
+  Tip.displayNoTips(noTipsMessage);
+};
+
+Tip.displayNoTips = function(message) {
+  const tipsList = document.getElementById('tipsList');
+  if (tipsList.childElementCount === 0) {
+    const noTips = document.createElement('p');
+    noTips.className = 'no-results mt-5 mb-5';
+    noTips.textContent = message;
+
+    tipsList.appendChild(noTips);
+  }
 };
