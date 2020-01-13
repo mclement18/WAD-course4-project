abc5671f5e6a8033660449ed2d37a4e268ec97f8
Mathieu Clément <mcfly1893@gmail.com>
2020-01-13 09:19:52 +0100

    Implement alert in comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,12 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
     respond_to do |format|
+      flash.clear
       if @comment.save
+        @comment_was_saved = true
         flash[:notice] = t('notices.comment_created')
       else
+        @comment_was_saved = false
         flash[:alert] = t('alerts.comment_creation_failed')
       end
       format.js
